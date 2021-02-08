a7098c46b84486d5266244f4b9639fb41db8e333
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-02-08 18:01:11 +0900

    Create Sequential Search basic.py

---
+++ algorithm/binary search/Sequential Search basic.py
@@ -0,0 +1,15 @@
+def sequential_search(n, target, array):
+  for i in range(n):
+    if array[i] == target:
+      return i+1
+  return "그런 값 없다."
+
+print("생성할 원소 개수 입력->찾을 문자열 입력")
+input_data = input().split()
+n = int(input_data[0])
+target = input_data[1]
+
+print("앞서 적은 원소 개수만큼 문자열을 입력하시오.")
+array = input().split()
+
+print(sequential_search(n,target,array))
