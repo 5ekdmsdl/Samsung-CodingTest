14998fd4f0e9f59afee7e72d77d3886a1b564aae
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-02-08 18:39:03 +0900

    Create answer.py

---
+++ algorithm/binary search/pb2 부품 찾기/answer.py
@@ -0,0 +1,31 @@
+#O( (N+M)*logN )
+
+def search_item(array,target,start,end):
+  while start <= end:
+    mid = (start+end)//2
+    if array[mid] == target:
+      return mid
+    elif array[mid] > target:
+      end = mid - 1
+    elif array[mid] < target:
+      start = mid + 1
+  return None
+#logN
+
+
+n = int(input())
+items = list(map(int,input().split()))
+items.sort()
+#NlogN
+
+
+m = int(input())
+requested = list(map(int,input().split()))
+
+for item in requested:
+  #M*logN
+  result = search_item(items,item,0,n-1)
+  # end = n - 1 이어야!!!!!!
+  if result == None:
+    print("no",end=' ')
+  else : print("yes",end=' ')
