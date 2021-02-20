69addab3eadb9d09a7798d8fc7ea486f6813f202
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-02-20 18:02:56 +0900

    Update mine.py

--- algorithm/greedy/Q1 모험가 길드/mine.py
+++ algorithm/greedy/Q1 모험가 길드/mine.py
@@ -1,1 +1,20 @@
-dd
+# n = int(input())
+n = 5*20000
+
+# data = list(map(int,input().split()))
+data = [2,3,1,2,2]*20000
+
+flag = False
+team_cnt = 0
+index = 0
+data.sort(reverse=True)
+
+while index < n:
+  val = data[index]
+  index += val
+  if index > n:
+    break
+  else:
+    team_cnt += 1
+    
+    print(team_cnt)
