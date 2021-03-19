b9fa22f37efadb09a5bcbbf04ec39421e1eb500a
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-03-19 22:09:05 +0900

    Update answer.py

--- algorithm/Dynamic Programming/Q34 병사 배치하기/answer.py
+++ algorithm/Dynamic Programming/Q34 병사 배치하기/answer.py
@@ -1,16 +1,13 @@
 n = int(input())
-array = list(map(int, input().split()))
-# 순서를 뒤집어 '최장 증가 부분 수열' 문제로 변환
-array.reverse()
+data = list(map(int,input().split()))
+data.reverse()
 
-# 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
-dp = [1] * n
+db = [0]*(n)
+db[0] = 1
 
-# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
-for i in range(1, n):
-    for j in range(0, i):
-        if array[j] < array[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+for i in range(1,n):
+  for j in range(i-1,-1,-1):
+    if data[j] < data[i]:
+      db[i] = max(db[i],db[j]+1)
 
-# 열외해야 하는 병사의 최소 수를 출력
-print(n - max(dp))
+print(n-db[-1])
