772e848ec4165012fb5e16d2c9149cdeabb888e5
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-06-21 23:25:06 +0900

    Create mine.py

---
+++ samsung/3/9 연산자 배치하기/mine.py
@@ -0,0 +1,34 @@
+def plus(a,b) :
+    return a+b
+def minus(a,b) :
+    # print(a,b)
+    return a-b
+def multiply(a,b) : return a*b
+
+calculate = [plus, minus, multiply]
+
+def func(result, abc, cnt):
+
+    global max_val, min_val
+    if cnt > n-1 :
+        if result > max_val :
+            max_val = result
+        if result < min_val : min_val = result
+        return
+    for i in range(3) :
+        if abc[i] :
+            abc[i] -= 1
+            # print(calculate[i](result, nums[cnt]), abc, cnt)
+            func(calculate[i](result, nums[cnt]), abc, cnt+1)
+            abc[i] += 1
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+abc = list(map(int, input().split()))
+
+min_val, max_val = int(1e9), -int(1e9)
+
+func(nums[0], abc, 1)
+
+print(min_val, max_val)
