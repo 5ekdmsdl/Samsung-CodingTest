1b02ba661a8830b642f9bcfbb8d4b34681f9c613
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-08-29 22:57:13 +0900

    Create reviewed.py

---
+++ samsung/6/21 생명과학부 랩 인턴/reviewed.py
@@ -0,0 +1,53 @@
+def get(c) :
+    global total
+    for x in range(n) :
+        if (x,c) in gs.keys() :
+            total += gs[(x,c)][0]
+            gs.pop((x,c))
+            break
+    return
+
+def move():
+    //gs[(x,y)] = [크기, 속도, 방향]
+    global gs
+    n_gs = {}
+    for xy in gs.keys() :
+        ox,oy = xy
+        x,y = xy
+        b,s,d = gs[xy]
+
+        //한 칸씩 움직이기 => 한번에 연산해서 옮기기!
+        for i in range(s) :
+            nx, ny = x+dx[d], y+dy[d]
+            if not (0 <= nx <= n-1 and 0 <= ny <= m-1) :
+                d = rev[d]
+                nx, ny = x + dx[d], y + dy[d]
+            x,y = nx, ny
+        //n_gs에 업데이트
+        //다른애랑 마주치면
+        if (x,y) in n_gs.keys() :
+            if n_gs[(x,y)][0] < b :
+                n_gs[(x,y)] = [b,s,d]
+        else : n_gs[(x,y)] = [b,s,d]
+    //gs 업데이트
+    gs = n_gs
+    return
+
+n,m, k = map(int, input().split())
+gs = {}
+
+for _ in range(k) :
+    x,y,s,d,b = map(int, input().split())
+    gs[(x-1,y-1)] = [b,s,d]
+
+dx, dy = [0,-1,1,0,0], [0,0,0,1,-1]
+rev = [0,2,1,4,3]
+total = 0
+
+if k != 0 :
+    for i in range(m) :
+        get(i)
+        move()
+
+// 100*1000
+print(total)
