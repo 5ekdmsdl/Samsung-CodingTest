2d85cdbb17b4f7e42faa7b8ba265507453f2171d
5ekdmsdl <76278018+5ekdmsdl@users.noreply.github.com>
2021-09-04 12:02:52 +0900

    Update mine.py

--- samsung/7/27윷놀이 사기단/mine.py
+++ samsung/7/27윷놀이 사기단/mine.py
@@ -1,64 +1,66 @@
-def catch(x,y):
-    global result, mapp, t
-    if mapp[x][y] :
-        result += mapp[x][y][0]
-        mapp[t[0]][t[1]] = 0
-        mapp[x][y] = 99
-        t = [x,y]
-    return
+from itertools import product
 
-def rotate(d,i):
-    d = d + i
-    if d < 0 :
-        d += 8
-    elif d > 7 : d -= 8
-    return d
+def run(p, step):
+    global ps, nodes
+    point = 0
+    pos = ps[p]
+    if pos == 5 or pos == 10 or pos == 15 :
+        pos = nodes[pos][1][1]
+        if pos == 99 :
+            return point
+        point += nodes[pos][0]
+        print(nodes[pos][0])
+        step -= 1
+    for s in range(step) :
 
-def runner():
-    num = 1
-    for x in range(4):
-        for y in range(4):
-            if mapp[x][y] == 99 : continue
-            if mapp[x][y][0] == num :
-                num += 1
-                for i in range(8):
-                    d = mapp[x][y][1]-1
-                    nx, ny = x+dx[d], y+dy[d]
-                    if (not 0<=nx<=3 and 0<=ny<=3) or mapp[nx][ny] == 99 :
-                        d = rotate(d, i)
-                        continue
-                    else :
-                        if mapp[x][y]:
-                            tmp = mapp[x][y]
-                            mapp[x][y] = mapp[nx][ny]
-                            mapp[nx][ny] = tmp
-                        else :
-                            mapp[nx][ny] = num
-                            mapp[x][y] = 0
-                        break
+        if pos == 5 or pos == 10 or pos == 15 :
+            pos = nodes[pos][1][0]
+        else:
+            pos = nodes[pos][1]
+        if pos == 99 :
+            return point
+        point += nodes[pos][0]
+        print(nodes[pos][0])
+
+    ps[p] = pos
+    return point
+
+def setnodes():
+    ns = [[0,1]]
+    for i in range(1,21):
+        ns.append([2*i, i+1])
+    ns.append([0,99]) #21번이 끝!, 25가 가운데
+    tmp = [[13,23],[16,24],[19,25]] #22 23 24
+    ns += tmp
+    ns.append([25,31])
+    tmp = [[22,27],[24,25]] #26 27
+    ns += tmp
+    tmp = [[28,29],[27,30],[26,25]] # 28 29 30
+    ns += tmp
+    tmp = [[30,32],[35,20]] # 31 32
+    ns += tmp
+    ns[5] = [10,[6,22]]
+    ns[10] = [20,[11,26]]
+    ns[15] = [30,[16,28]]
+    return ns
 
 
+ps = [0,0,0,0]
+nodes = setnodes()
+turns = list(product(list(range(1,5)),repeat=10))
+answer = 0
+steps = list(map(int, input().split()))
+onEnd = []
 
-    return
+for turn in turns :
+    total = 0
+    for i in range(10):
+        total += run(turn[i], steps[i])
+    if total > answer :
+        answer = total
 
-def catcher():
-    return
-
-def game(mapp, result):
-    runner()
-    # catcher()
-
-dx, dy = [-1,-1,0,1,1,1,0,-1], [0,-1,-1,-1,0,1,1,1]
-mapp = [[] for _ in range(4)]
-result, answer = 0, 0
-t = [0,0]
-for i in range(4):
-    line = list(map(int, input().split()))
-    for j in range(4):
-        mapp[i].append([line[j*2], line[j*2+1]])
-
-catch(0,0)
-
-game(mapp, result)
-
-# runner()
+# idx =
+# for i in range(9):
+#     n = nodes[idx]
+#     print(n[0])
+#     idx = n[1]
